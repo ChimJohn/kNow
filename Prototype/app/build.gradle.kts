7927f6b93da2a4a5d132ba16b627dda4b235b51c
Joel Yeo <44794609+joelchunsheng@users.noreply.github.com>
2025-03-09 04:35:32 +0800

    Registration Completed

--- Prototype/app/build.gradle.kts
+++ Prototype/app/build.gradle.kts
@@ -37,6 +37,13 @@ android {
 }
 
 dependencies {
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
 
