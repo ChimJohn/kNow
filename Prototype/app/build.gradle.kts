d06383002bd34dc2edab9ccdcff0155b94749680
how2fps <hearthstonesigh@gmail.com>
2025-03-09 12:08:55 +0800

    updated dependencies
    start using version catalog to clean up gradle build - check libs.versions.toml

--- Prototype/app/build.gradle.kts
+++ Prototype/app/build.gradle.kts
@@ -38,34 +38,35 @@ android {
 
 dependencies {
     // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
+    implementation(platform(libs.firebase.bom))
 
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation("com.google.firebase:firebase-auth")
+    implementation(libs.google.firebase.auth)
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation(libs.appcompat.v161)
+    implementation(libs.play.services.maps.v1820)
 
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation(libs.constraintlayout.v221)
+    implementation(libs.material.v180)
+    implementation(libs.activity.ktx)
     implementation(libs.camera.core)
     implementation(libs.camera.lifecycle)
     implementation(libs.camera.view)
 
-    implementation ("androidx.camera:camera-core:1.3.0")
-    implementation ("androidx.camera:camera-camera2:1.3.0")
-    implementation ("androidx.camera:camera-lifecycle:1.3.0")
-    implementation ("androidx.camera:camera-view:1.3.0")
+    implementation (libs.camera.core.v130)
+    implementation (libs.androidx.camera.camera2)
+    implementation (libs.androidx.camera.lifecycle.v130)
+    implementation (libs.androidx.camera.view.v130)
     implementation(libs.firebase.auth)
     implementation(libs.credentials)
     implementation(libs.credentials.play.services.auth)
     implementation(libs.googleid)
+    implementation(libs.firebase.common)
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit.v115)
+    androidTestImplementation(libs.androidx.espresso.core.v351)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
